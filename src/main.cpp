1e65ea487d2231c6dc0b4fd64400d7d83c7cdcfb
Fardin Fahim <sc19ff@leeds.ac.uk>
2021-09-12 14:43:37 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "models.h"
 #include "collisions.h"
 
+#define NDEBUG true
+
 constexpr auto SCREEN_WIDTH = 1600;
 constexpr auto SCREEN_HEIGHT = 900;
 
@@ -23,19 +25,25 @@ int main() {
         ClearBackground(RAYWHITE);
         Draw3DObjects();
 
+#ifndef NDEBUG
         //Debug Text
-        DrawText(TextFormat("X: %f", playerPos.x), 3, 0, 20, WHITE);
-        DrawText(TextFormat("Y: %f", playerPos.y), 3, 21, 20, WHITE);
-        DrawText(TextFormat("Z: %f", playerPos.z), 3, 42, 20, WHITE);
-        DrawText(TextFormat("V: %f", velocity), 3, 63, 20, WHITE);
+        DrawText(TextFormat("X: %f", playerPos.x), 3, 21, 20, WHITE);
+        DrawText(TextFormat("Y: %f", playerPos.y), 3, 42, 20, WHITE);
+        DrawText(TextFormat("Z: %f", playerPos.z), 3, 63, 20, WHITE);
+        DrawText(TextFormat("V: %f", velocity), 3, 84, 20, WHITE);
         DrawText(TextFormat("SIDEWAYS V: %f", sidewaysVelocity), 3, 84, 20, GRAY);
         DrawText(TextFormat("TIME TILL TUMBLE: %f", (lastTumbleTime + tumbleInterval) - GetTime() ), 3, 105, 20, GRAY);
-
+#endif
         // UI TEXT
         DrawText(TextFormat("DRUNKNESS : %s", ((drunkTier == SOBER) ? "SOBER" : ((drunkTier == TIPSY) ? "TIPSY" : "NAUSEOUS"))),
-                            3, 126, 20,
+                            3, 0, 20,
                             (drunkTier == SOBER) ? GREEN : (drunkTier == TIPSY) ? YELLOW : RED);        
         
+        if (drunkTier == NAUSEOUS) {
+            DrawText(TextFormat("GET SOBER OR CAUSE SOME CARNAGE"), SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT - 10, 40, DARKPURPLE);
+            DrawText(TextFormat("Puke Countdown : %i", static_cast<int>(nauseousCountdown)), SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT - 10, 40, DARKPURPLE);
+        }
+    
         EndDrawing();
     }
 
